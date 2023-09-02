96bee7a209a411091cce60fd40233ecd621e770f
Denis Kirdyankin <kird.den@rambler.ru>
2023-09-02 16:51:33 +0300

    new function mask

---
+++ utils/date_from.py
@@ -0,0 +1,12 @@
+from datetime import datetime
+
+
+def format_time(str_date):
+    '''
+    :param str_date: str data with '%Y-%m-%dT%H:%M:%S.%f'
+    :return: str with '%d.%m.%Y'
+    '''
+    d_obj = datetime.strptime(str_date, '%Y-%m-%dT%H:%M:%S.%f')
+    date_true = d_obj.strftime('%d.%m.%Y')
+    return date_true
+
